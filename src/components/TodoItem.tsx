65433d30a2352ca58625a4244f1e3c59d247548a
Leo <drd9811@naver.com>
2023-03-01 16:31:42 +0900

    Input Element 로 Todo Text 받기

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,9 @@
 import React, {FC} from "react";
+import classes from './TodoItem.module.css';
 
 const TodoItem: FC<{ text: string }> = (props) => {
     return (
-        <li>{props.text}</li>
+        <li className={classes.item}>{props.text}</li>
     );
 };
 
